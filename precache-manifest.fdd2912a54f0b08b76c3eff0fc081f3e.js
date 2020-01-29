fcc5cc49b94b521e9eec0b519010043a9dc079b8
hector angulo <hectorangulo@macbook-pro.lan>
2020-01-29 14:20:47 -0800

    Updates

---
+++ precache-manifest.fdd2912a54f0b08b76c3eff0fc081f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f33235f95baae61022c997ab49f9b54",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f7efa39c39ffc9bed64a",
+    "url": "/monster-rolodex/static/css/main.00a41edd.chunk.css"
+  },
+  {
+    "revision": "662a3abb5f9258fa8b66",
+    "url": "/monster-rolodex/static/js/2.1cc6cf39.chunk.js"
+  },
+  {
+    "revision": "f7efa39c39ffc9bed64a",
+    "url": "/monster-rolodex/static/js/main.38a2c4e8.chunk.js"
+  },
+  {
+    "revision": "000fb06d0e805fa286b7",
+    "url": "/monster-rolodex/static/js/runtime-main.1ab9c4fb.js"
+  }
+]);
